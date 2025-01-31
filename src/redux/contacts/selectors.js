e5197138a7c64b9fe1ba148296f06b157daa6cab
Alyona <olenapav97@gmail.com>
2025-01-31 14:38:49 +0200

    GF

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -2,15 +2,15 @@ import { createSelector } from "@reduxjs/toolkit";
 import { selectNameFilter } from "../filters/selectors";
 
 export const selectContacts = (state) => state.contacts.items;
-export const selectIsLoading = (state) => state.contacts.loading;
+
+export const selectLoading = (state) => state.contacts.loading;
+
 export const selectError = (state) => state.contacts.error;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
-  (contacts, nameFilter) => {
-    const normalizedFilter = nameFilter?.toLowerCase() || "";
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  }
+  (contacts, nameFilter) =>
+    contacts?.filter((contact) =>
+      contact.name.toLowerCase().includes(nameFilter?.toLowerCase())
+    )
 );
