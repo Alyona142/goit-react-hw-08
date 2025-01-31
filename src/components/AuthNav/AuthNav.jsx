f9cb1612c9144b5a9821feea6430b0abc48d84da
Alyona <olenapav97@gmail.com>
2025-01-31 22:36:12 +0200

    dfczx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom";
-import css from "./AuthNav.module.css";
+import styles from "./AuthNav.module.css";
 
 export const AuthNav = () => {
   return (
     <div>
-      <div className={css["auth-nav-container"]}>
-        <NavLink className={css.link} to="/register">
+      <div className={styles.navContainer}>
+        <NavLink className={styles.nameLink} to="/register">
           Register
         </NavLink>
-        <NavLink className={css.link} to="/login">
+        <NavLink className={styles.nameLink} to="/login">
           Login
         </NavLink>
       </div>
